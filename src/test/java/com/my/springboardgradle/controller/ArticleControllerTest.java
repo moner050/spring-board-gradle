2a465eab6654614e3c044b7801e470b942490dac
moner050 <minhyung0237@gmail.com>
2023-02-20 22:41:54 +0900

    #15 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 얻어올지 테스트로 명세서를 뽑음
    기존 테스트를 실패한 채 올리면 `gradle.build`가 실패하여
    배포 자동화에 악영향을 미치는 경우가 있으므로,
    실패 테스트는 테스트 제외 처리하는 방향으로 정책을
    잡아보려고 했다.
    이에 기능 구현중인 유닛 테스트를 모두 `@Disabled` 처리
    그 밖에 추가로 생각난 테스트 방법을 집어넣음.

---
+++ src/test/java/com/my/springboardgradle/controller/ArticleControllerTest.java
@@ -0,0 +1,89 @@
+package com.my.springboardgradle.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[View][Get] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
+        // Given
+
+        // When&Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                // 타입이 TEXT_HTML 인지 확인
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                // 이 URL 에 해당한 뷰가 있어야 한다.
+                .andExpect(view().name("articles/index"))
+                // model 에 해당 키의 데이터가 있는지 확인
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[View][Get] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
+        // Given
+
+        // When&Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                // 타입이 TEXT_HTML 인지 확인
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                // 이 URL 에 해당한 뷰가 있어야 한다.
+                .andExpect(view().name("articles/detail"))
+                // model 에 해당 키의 데이터가 있는지 확인
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[View][Get] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+        // Given
+
+        // When&Then
+        mvc.perform(get("/article/search"))
+                .andExpect(status().isOk())
+                // 타입이 TEXT_HTML 인지 확인
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                // 이 URL 에 해당한 뷰가 있어야 한다.
+                .andExpect(view().name("articles/search"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[View][Get] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+        // Given
+
+        // When&Then
+        mvc.perform(get("/article/search-hashtag"))
+                .andExpect(status().isOk())
+                // 타입이 TEXT_HTML 인지 확인
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                // 이 URL 에 해당한 뷰가 있어야 한다.
+                .andExpect(view().name("articles/search-hashtag"));
+    }
+}
