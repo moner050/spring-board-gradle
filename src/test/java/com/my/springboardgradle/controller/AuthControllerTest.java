6fd17a4e6467421e1c320981b529db534e3c54fd
moner050 <minhyung0237@gmail.com>
2023-02-25 20:57:33 +0900

    #15 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동으로 생성해준 `/login` 컨트롤러 테스트
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 
    `@import` 추가.

---
+++ src/test/java/com/my/springboardgradle/controller/AuthControllerTest.java
@@ -0,0 +1,38 @@
+package com.my.springboardgradle.controller;
+
+import com.my.springboardgradle.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@DisplayName("View 컨트롤러 - 인증부분")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[View][Get] 로그인 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenTryingToLogin_thenReturnLoginView() throws Exception {
+        // Given
+
+        // When&Then
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                // 타입이 TEXT_HTML 인지 확인
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
